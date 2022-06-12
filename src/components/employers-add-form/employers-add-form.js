ae0ead2425ea47123d3c227c8380cb3edca82e5f
Nikita <ndemidovn@gmail.com>
2022-06-12 12:10:55 +0300

    form add

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -16,6 +16,7 @@ class EmployersAddForm extends Component {
   }
   onSubmit = (e) => {
     e.preventDefault();
+    if (this.state.name.length < 3 || !this.state.salary) return;
     this.props.onAdd(this.state.name, this.state.salary);
     this.setState({
       name: '',
